e0a5330b8d0c0750779cd02296aab7d096ff42a5
Rama Bondan Prakoso <ramanarubp@gmail.com>
2025-03-11 17:04:50 +0700

    X6855: Move patches to device
    Signed-off-by: Rama Bondan Prakoso <ramanarubp@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -60,7 +60,7 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
 	# Patches
 	RET=0
 	cd bootable/recovery
-	git apply ../../device/transsion/mt6789-common/patches/0001-Change-haptics-activation-file-path.patch > /dev/null 2>&1 || RET=$?
+	git apply ../../device/infinix/Infinix-X6855/patches/0001-Change-haptics-activation-file-path.patch > /dev/null 2>&1 || RET=$?
 	cd ../../
 	if [ $RET -ne 0 ];then
 	    echo "ERROR: Patch is not applied! Maybe it's already patched?"
